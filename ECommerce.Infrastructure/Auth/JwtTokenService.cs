2b3cf15f74cc5a1536d92efe7c87c1cd35e3799b
Nicolas Vallejo <>
2025-08-09 15:47:18 -0500

    fix: se agregaron referencias faltantes

--- ECommerce.Infrastructure/Auth/JwtTokenService.cs
+++ ECommerce.Infrastructure/Auth/JwtTokenService.cs
@@ -2,6 +2,7 @@ using System.IdentityModel.Tokens.Jwt;
 using System.Security.Claims;
 using System.Text;
 using Microsoft.IdentityModel.Tokens;
+using Microsoft.Extensions.Configuration;
 
 namespace ECommerce.Infrastructure.Auth;
 
